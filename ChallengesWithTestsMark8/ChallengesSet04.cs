e36c1dd620f2c3bbf97fcb4c1fc3e7f75890e806
Bryan Levin <bryanlevin72@gmail.com>
2023-07-22 11:10:47 -0700

    Weekly Challenge Set 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +7,122 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int answer = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    answer += numbers[i];
+                }
+                else
+                {
+                    answer -= numbers[i];
+                }
+            }
+            return answer;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int lengthOne = str1.Length;
+            int lengthTwo = str2.Length;
+            int lengthThree = str3.Length;
+            int lengthFour = str3.Length;
+
+            int shortestLength = new[] { str1.Length, str2.Length, str3.Length, str4.Length }.Min();
+            return shortestLength;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int smallestNumber = new[] { number1, number2, number3, number4 }.Min();
+            return smallestNumber;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return (sideLength1 + sideLength2 > sideLength3 &&
+                    sideLength2 + sideLength3 > sideLength1 &&
+                    sideLength3 + sideLength1 > sideLength2);
+
+            //if (sideLength1 + sideLength2 > sideLength3 &&
+            //   sideLength2 + sideLength3 > sideLength1 &&
+            //   sideLength3 + sideLength1 > sideLength2)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
         }
+
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            double n;
+            bool isNumber = double.TryParse(input, out n);
+            return isNumber;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int objsNull = objs.Count(o => o is null);
+            int objsNotNull = objs.Count(o => o is not null);
+            //int objsNull = 0;
+            //int objsNotNull = 0;
+
+            //foreach(object o in objs)
+            //{
+            //    if(objs is null)
+            //    {
+            //        objsNull++;
+            //    }
+            //    else
+            //    {
+            //        objsNotNull++;
+            //    }
+            //}
+            return objsNull > objsNotNull;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return numbers.Where(n => n % 2 == 0).Average();
+            }
+            catch (ArgumentNullException)
+            {
+                return 0;
+            }
+            catch (InvalidOperationException)
+            {
+                return 0;
+            }
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if(number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if(number == 1 || number == 0)
+            {
+                return 1;
+            }
+            if(number >= 2)
+            {
+                var factorial = number * Factorial(number - 1);
+                return factorial;
+            }
+            return 0;
         }
     }
 }
